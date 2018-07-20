49acd81f9eafdd6dcaebd8e275d43e4517c9561e
kkoss <kristie-lynne.koss@getbraintree.com>
2018-07-20 15:07:23 -0500

    Remove timer for now

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from '../Home/Home'
 import {StackNavigator} from 'react-navigation'
-import Timer from '../Timer/Timer'
 import Human from '../Human/Human'
 import Create from '../Create/Create'
 
@@ -13,7 +12,6 @@ const Stack = StackNavigator({
 export const Layout = StackNavigator(
   {
     Main: {screen: Stack},
-    Timer: {screen: Timer},
     Create: {screen: Create},
   },
   {
